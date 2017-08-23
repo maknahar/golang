dee833ad3ad6ca5a05fe1432140acf4633cc095a
maknahar <maknahar@yahoo.in>
2017-08-23 18:14:23 +0530

    Vendorised dependencies

--- vendor/github.com/namsral/flag/examples/gopher.go
+++ vendor/github.com/namsral/flag/examples/gopher.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"github.com/namsral/flag"
-)
+    "github.com/namsral/flag"
+    "fmt"
+    )
 
 func main() {
-	var (
-		config string
-		length float64
-		age    int
-		name   string
-		female bool
-	)
+    var (
+        config string
+        length float64
+        age int
+        name string
+        female bool
+    )
 
-	flag.StringVar(&config, "config", "", "help message")
-	flag.StringVar(&name, "name", "", "help message")
-	flag.IntVar(&age, "age", 0, "help message")
-	flag.Float64Var(&length, "length", 0, "help message")
-	flag.BoolVar(&female, "female", false, "help message")
-
-	flag.Parse()
-
-	fmt.Println("length:", length)
-	fmt.Println("age:", age)
-	fmt.Println("name:", name)
-	fmt.Println("female:", female)
+    flag.StringVar(&config, "config", "", "help message")
+    flag.StringVar(&name, "name", "", "help message")
+    flag.IntVar(&age, "age", 0, "help message")
+    flag.Float64Var(&length, "length", 0, "help message")
+    flag.BoolVar(&female, "female", false, "help message")
+    
+    flag.Parse()
+    
+    fmt.Println("length:", length)
+    fmt.Println("age:", age)
+    fmt.Println("name:", name)
+    fmt.Println("female:", female)
 }
