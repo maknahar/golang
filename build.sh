d96943d0ecc15926da3c4636cfe35495787fdd2a
maknahar <maknahar@yahoo.in>
2017-08-23 17:33:12 +0530

    Added build file

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+echo "Setting env vars"
+#TODO
+source $GOPATH/src/github.com/maknagar/go-web-skelton/dev.env
+
+echo "Doing some cleaning ..."
+go clean
+echo "Done."
+
+echo "Running goimport ..."
+goimports -w=true .
+echo "Done."
+
+echo "Running go vet ..."
+go vet ./...
+if [ $? != 0 ]; then
+  exit
+fi
+echo "Done."
+
+echo "Running go generate ..."
+go generate ./...
+echo "Done."
+
+echo "Running go format ..."
+gofmt -w .
+echo "Done."
+
+echo "Running go build ..."
+go build -race
+if [ $? != 0 ]; then
+  echo "## Build Failed ##"
+  exit
+fi
+echo "Done."
+
+echo "Running unit test ..."
+go test -p=1 -v
+if [ $? == 0 ]; then
+    echo "Done."
+	echo "## Starting service ##"
+	#TODO
+    ./go-web-skeleton -v
+fi
